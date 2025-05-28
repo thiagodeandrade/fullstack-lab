6c3ba00e3b9bdb34fd5463763f858db039a6ae67
Thiago Felipe de Andrade <thiago@qxs.local>
2025-05-28 18:16:30 -0300

    Commit to cicd

--- infra-jmeter/outputs.tf
+++ infra-jmeter/outputs.tf
@@ -2,9 +2,3 @@ output "jmeter_public_ip" {
   description = "Public IP address of the JMeter server"
   value       = digitalocean_droplet.jmeter.ipv4_address
 }
-output "ip_address" {
-  value = digitalocean_droplet.app.ipv4_address
-}
-output "jmeter_ip" {
-  value = digitalocean_droplet.jmeter.ipv4_address
-}
