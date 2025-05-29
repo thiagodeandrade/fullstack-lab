98159b947578a7b48d61f2199e252f49c35f8bba
Thiago Felipe de Andrade <thiago@qxs.local>
2025-05-29 20:58:42 -0300

    fixes

--- infra/provision.sh
+++ infra/provision.sh
@@ -35,8 +35,58 @@ server {
     location / {
         try_files \$uri \$uri/ =404;
     }
+
+    location /prometheus/ {
+        auth_basic "Restricted";
+        auth_basic_user_file /etc/nginx/.htpasswd;
+        proxy_pass http://localhost:9090/;
+        proxy_set_header Host \$host;
+    }
+
+    location /metrics/ {
+        auth_basic "Restricted";
+        auth_basic_user_file /etc/nginx/.htpasswd;
+        proxy_pass http://localhost:9100/;
+        proxy_set_header Host \$host;
+    }
 }
 EOF
 
+apt install -y apache2-utils
+htpasswd -cb /etc/nginx/.htpasswd admin lablab2025...
+systemctl reload nginx
+
+
 # Restart NGINX
 systemctl restart nginx
+
+# Instala o Node Exporter no app-server
+useradd --no-create-home --shell /usr/sbin/nologin node_exporter
+
+NODE_EXPORTER_VERSION="1.7.0"
+cd /tmp
+curl -LO https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+tar xvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+cp node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin/
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+# Cria o service systemd
+cat <<EOF > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+ExecStart=/usr/local/bin/node_exporter
+Restart=always
+
+[Install]
+WantedBy=default.target
+EOF
+
+# Inicia o serviço
+systemctl daemon-reexec
+systemctl daemon-reload
+systemctl enable --now node_exporter
